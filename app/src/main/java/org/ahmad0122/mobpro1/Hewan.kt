668657f110d4cd48ba114df17bc0bd59484c0308
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-03-04 20:20:13 +0700

    Menambahkan Data Hewan

---
+++ app/src/main/java/org/ahmad0122/mobpro1/Hewan.kt
@@ -0,0 +1,8 @@
+package org.ahmad0122.mobpro1
+
+import androidx.annotation.DrawableRes
+
+data class Hewan(
+    val nama: String,
+    @DrawableRes val imageResId: Int
+)
--- app/src/main/java/org/ahmad0122/mobpro1/MainActivity.kt
+++ app/src/main/java/org/ahmad0122/mobpro1/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +29,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val data = listOf(
+        Hewan("Ayam", R.drawable.ayam),
+        Hewan("Bebek", R.drawable.bebek),
+        Hewan("Domba", R.drawable.domba),
+        Hewan("Kambing", R.drawable.kambing),
+        Hewan("Sapi", R.drawable.sapi),
+    )
 }
 
 @Composable
