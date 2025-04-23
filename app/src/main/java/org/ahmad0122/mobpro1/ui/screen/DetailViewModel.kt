277b77ba45563d8b8af51a697fc98845a1732c4a
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-04-23 13:40:25 +0700

    Menyimpan data ke database

---
+++ app/src/main/java/org/ahmad0122/mobpro1/ui/screen/DetailViewModel.kt
@@ -0,0 +1,30 @@
+package org.ahmad0122.mobpro1.ui.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.ahmad0122.mobpro1.database.CatatanDao
+import org.ahmad0122.mobpro1.model.Catatan
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class DetailViewModel(private val dao: CatatanDao) : ViewModel() {
+    private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+
+    fun insert(judul: String, isi: String) {
+        val catatan = Catatan(
+            tanggal = formatter.format(Date()),
+            judul = judul,
+            catatan = isi
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.insert(catatan)
+        }
+    }
+    fun getCatatan(id: Long): Catatan? {
+        return null
+    }
+}
--- app/src/main/java/org/ahmad0122/mobpro1/util/ViewModelFactory.kt
+++ app/src/main/java/org/ahmad0122/mobpro1/util/ViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.ahmad0122.mobpro1.MainViewModel
 import org.ahmad0122.mobpro1.database.CatatanDb
+import org.ahmad0122.mobpro1.ui.screen.DetailViewModel
 
 
 class ViewModelFactory(
@@ -16,7 +17,9 @@ class ViewModelFactory(
 
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(dao) as T
-        }
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+                return DetailViewModel(dao) as T
+            }
 
         throw IllegalArgumentException("Unknown ViewModel class")
     }
