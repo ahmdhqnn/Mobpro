7ee7b5d44eafe676c962ac7f9df2415cd830e72a
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-04-23 15:31:40 +0700

    fix bug

--- app/src/main/java/org/ahmad0122/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/ahmad0122/mobpro1/ui/screen/MainScreen.kt
@@ -4,12 +4,13 @@ import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
