55c21ed7e9a734278f302a6a17517bb9d047aebf
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-05-08 22:38:52 +0700

    ases2wip-done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.gson)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
--- app/src/main/java/org/ahmad0122/mobpro1/MainActivity.kt
+++ app/src/main/java/org/ahmad0122/mobpro1/MainActivity.kt
@@ -3,17 +3,26 @@ package org.ahmad0122.mobpro1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import org.ahmad0122.mobpro1.navigation.SetUpNavGraph
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import org.ahmad0122.mobpro1.navigation.ContactNavigation
 import org.ahmad0122.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Mobpro1Theme {
-                SetUpNavGraph()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    ContactNavigation(navController)
+                }
             }
         }
     }
